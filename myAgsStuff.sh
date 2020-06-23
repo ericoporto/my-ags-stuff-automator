5da368635c7391b732c8f0d23902680ad8afd1f2
ericoporto <ericoporto2008@gmail.com>
2020-06-23 20:48:55 -0300

    fixes that allow overriding ciname for azure

--- myAgsStuff.sh
+++ myAgsStuff.sh
@@ -4,34 +4,34 @@ intro="Using AGS for some time now, here's a list of things I made!"
 footnote=("Thank you for reading this list. If something is broken or needs changes, open an issue or hit me up on the forums." "https://gist.github.com/ericoporto/a3d437af481a14609cfbef95e7a9a293" "https://gist.githubusercontent.com/ericoporto/6426d9e819f4eccb74c4f47f6d7dfcb5/raw/93007937e87d7de6e35b29382e1bfbe49bb5cc90/README.BBCode" "https://github.com/ericoporto/my-ags-stuff-automator")
   
 scriptModules=(
-    "math3d" "A script module for 3D math on Adventure Game Studio." 
-    "rellax" "Rellax camera tracks with parallax, adds smooth scroll."
-    "controlz" "Move your character with keyboard or joystick controlz for Adventure Game Studio."
-    "arrowselect" "Select things using arrows, module for in point and click games made with Adventure Game Studio."
-    "ags_test_game" "Example of Test Anything Protocol usage with Adventure Game Studio."
-    "jsmnags" "JSMN, a JSON minimal parser, ported for Adventure Game Studio." 
-    "resizablegui" "Resizable GUIs module for Adventure Game Studio with optional mouse controls for resizing."
-    "SierraSpeech" "AGS module for Sierra style conversations forked from speech bubble."
-    "adventureJoy" "module for easily enabling joystick support on an Adventure Game Studio project (requires agsjoy plugin)."
-    "NormRnd" "An Adventure Game Studio module to generate standard normal random numbers."
+    "math3d" "A script module for 3D math on Adventure Game Studio."  ""
+    "rellax" "Rellax camera tracks with parallax, adds smooth scroll." ""
+    "controlz" "Move your character with keyboard or joystick controlz for Adventure Game Studio." ""
+    "arrowselect" "Select things using arrows, module for in point and click games made with Adventure Game Studio." ""
+    "ags_test_game" "Example of Test Anything Protocol usage with Adventure Game Studio." ""
+    "jsmnags" "JSMN, a JSON minimal parser, ported for Adventure Game Studio."  ""
+    "resizablegui" "Resizable GUIs module for Adventure Game Studio with optional mouse controls for resizing." ""
+    "SierraSpeech" "AGS module for Sierra style conversations forked from speech bubble." ""
+    "adventureJoy" "module for easily enabling joystick support on an Adventure Game Studio project (requires agsjoy plugin)." ""
+    "NormRnd" "An Adventure Game Studio module to generate standard normal random numbers." ""
 ) 
 
 agsPluginsWithAzureIntegration=(
-    "agsimgui" "An ImGui plugin for Adventure Game Studio, so you can create beautiful and fast GUIs using AGS Script."
-    "agsbox2d" "AgsBox2D is a physics plugin for Adventure Game Studio that gives access to the Box2D library created by Erin Catto. API is more inspired in the one used in Löve than the original Box2D one."
-    "agssqlite" "AGS Fast Wave Function Collapse Plugin, makes magical self fiting tile patterns!" 
-    "agsfastwfc" "SQLite plugin for Adventure Game Studio, adds way to store and retrieve information from local SQLite databases!"
+    "agsimgui" "An ImGui plugin for Adventure Game Studio, so you can create beautiful and fast GUIs using AGS Script." ""
+    "agsbox2d" "AgsBox2D is a physics plugin for Adventure Game Studio that gives access to the Box2D library created by Erin Catto. API is more inspired in the one used in Löve than the original Box2D one." ""
+    "agssqlite" "AGS Fast Wave Function Collapse Plugin, makes magical self fiting tile patterns!"  ""
+    "agsfastwfc" "SQLite plugin for Adventure Game Studio, adds way to store and retrieve information from local SQLite databases!" ""
 )
  
 agsEditorPluginsWithAzureIntegration=(
-    "agsModuleList" "includes both the agsModuleList website and the agsget Editor Plugin, which makes finding and downloading AGS Modules easy. If you want to update or add your module, open a PR. Any questions, open an issue."
+    "agsModuleList" "includes both the agsModuleList website and the agsget Editor Plugin, which makes finding and downloading AGS Modules easy. If you want to update or add your module, open a PR. Any questions, open an issue." "agsget"
 )
 
 forksWithAzureIntegration=(
-    "AGS-Controller" "AGS-Controller is a SDL2 Gamepad plugin for AGS, this fork has CI integration to provide automatic cross platform builds."
-    "AGSWaves" "AGSWaves provides helpful features to work around ags3 sound limitation and other things. This fork has CI integration to provide automatic cross platform builds."
-    "agslua" "agslua plugin adds lua support to 3.x AGS Editor. This fork adds CI integration to provide builds for Linux and MacOS."
-    "AGSNormalMap" "Adds normalmaps for AGS! This fork adds CI integration to provide cross platform builds."    
+    "AGS-Controller" "AGS-Controller is a SDL2 Gamepad plugin for AGS, this fork has CI integration to provide automatic cross platform builds." "agscontroller"
+    "AGSWaves" "AGSWaves provides helpful features to work around ags3 sound limitation and other things. This fork has CI integration to provide automatic cross platform builds." ""
+    "agslua" "agslua plugin adds lua support to 3.x AGS Editor. This fork adds CI integration to provide builds for Linux and MacOS." ""
+    "AGSNormalMap" "Adds normalmaps for AGS! This fork adds CI integration to provide cross platform builds."  "agsnormalmaps"
 )
 
 forksWithCirrusCiIntegration=(
@@ -87,16 +87,21 @@ function writeFootNote() {
  
 function printWithAzurePipelines() {
    arr=("$@")
-   for ((i=0;i< ${#arr[@]} ;i+=2));
+   for ((i=0;i< ${#arr[@]} ;i+=3));
       do    
           reponame="${arr[i]}"
           description="${arr[i+1]}"
+          ciname="${arr[i+2]}"
+          if [ "$ciname" == "" ]; then   
+            ciname="${reponame}"
+          fi          
+          
           if [ ${DO_MARKDOWN} -eq 1 ]; then       
-              echo "- [${reponame}](https://github.com/ericoporto/${reponame}) | [![Build Status](https://dev.azure.com/ericoporto/${reponame}/_apis/build/status/ericoporto.${reponame}?branchName=master)](https://dev.azure.com/ericoporto/${reponame}/_build/latest?definitionId=12&branchName=master) | [**\`Downloads ⇩\`**](https://github.com/ericoporto/${reponame}/releases)"
+              echo "- [${reponame}](https://github.com/ericoporto/${reponame}) | [![Build Status](https://dev.azure.com/ericoporto/${ciname}/_apis/build/status/ericoporto.${reponame}?branchName=master)](https://dev.azure.com/ericoporto/${ciname}/_build/latest?definitionId=12&branchName=master) | [**\`Downloads ⇩\`**](https://github.com/ericoporto/${reponame}/releases)"
               echo "  - ${description}"
               echo ""
           else
-              echo "[list][li][b][url=https://github.com/ericoporto/${reponame}]${reponame}[/url][/b]| [url=https://dev.azure.com/ericoporto/${reponame}/_build/latest?definitionId=12&branchName=master][img]https://dev.azure.com/ericoporto/${reponame}/_apis/build/status/ericoporto.${reponame}?branchName=master[/img][/url] | [u][url=https://github.com/ericoporto/${reponame}/releases][tt]Downloads ⇩[/tt][/url][/u][/li][list]
+              echo "[list][li][b][url=https://github.com/ericoporto/${reponame}]${reponame}[/url][/b]| [url=https://dev.azure.com/ericoporto/${ciname}/_build/latest?definitionId=12&branchName=master][img]https://dev.azure.com/ericoporto/${ciname}/_apis/build/status/ericoporto.${reponame}?branchName=master[/img][/url] | [u][url=https://github.com/ericoporto/${reponame}/releases][tt]Downloads ⇩[/tt][/url][/u][/li][list]
 [li]${description}[/li]
 [/list][/list]"         
           fi
@@ -105,10 +110,11 @@ function printWithAzurePipelines() {
 
 function printWithCirrusCiPipelines() {
    arr=("$@")
-   for ((i=0;i< ${#arr[@]} ;i+=2));
+   for ((i=0;i< ${#arr[@]} ;i+=3));
       do      
           reponame="${arr[i]}"
           description="${arr[i+1]}"
+             
           if [ ${DO_MARKDOWN} -eq 1 ]; then   
               echo "- [${reponame}](https://github.com/ericoporto/${reponame}) | [![Build Status](https://api.cirrus-ci.com/github/ericoporto/${reponame}.svg)](https://cirrus-ci.com/github/ericoporto/${reponame})"
               echo "  - ${description}"
@@ -123,10 +129,11 @@ function printWithCirrusCiPipelines() {
 
 function printWithReleasesOnly() {
    arr=("$@")
-   for ((i=0;i< ${#arr[@]} ;i+=2));
+   for ((i=0;i< ${#arr[@]} ;i+=3));
       do
           reponame="${arr[i]}"
           description="${arr[i+1]}"
+          
           if [ ${DO_MARKDOWN} -eq 1 ]; then  
               echo "- [${reponame}](https://github.com/ericoporto/${reponame}) | [**\`Downloads ⇩\`**](https://github.com/ericoporto/${reponame}/releases)"
               echo "  - ${description}"
